c035d0c00766d92c4e0b38499edc27fca99adda0
julioluque <julio.a.luque@gmail.com>
2020-09-14 00:38:46 -0300

    Commit inicial - Configuraciones OK. Proyecto listo para deployar

---
+++ src/main/java/com/jluque/springboot/app/zuul/SpringbootServicioZuulServerApplication.java
@@ -0,0 +1,17 @@
+package com.jluque.springboot.app.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class SpringbootServicioZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioZuulServerApplication.class, args);
+	}
+
+}
